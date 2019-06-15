34593da83685bd2c0d5b4bfdf4a3dd36277e84fc
Josinaldo Gomes <josinaldorobertorocha@gmail.com>
2019-06-15 20:36:17 -0300

    toString de Pedido implementado.

--- src/main/java/com/josinaldo/cursomc/domain/Pedido.java
+++ src/main/java/com/josinaldo/cursomc/domain/Pedido.java
@@ -1,8 +1,11 @@
 package com.josinaldo.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -133,6 +136,28 @@ public class Pedido implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt","BR"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy hh:mm:ss");
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido número: ");
+		builder.append(getId());
+		builder.append("\nInstante: ");
+		builder.append(sdf.format(getInstante()));
+		builder.append("\nCliente: ");
+		builder.append(getCliente().getNome());
+		builder.append("\nSituação do pagamento: ");
+		builder.append(getPagamento().getEstado().getDescricao());
+		builder.append("\nItens do pedido:\n");
+		for (ItemPedido ip : getItens()) {
+			builder.append(ip.toString());
+		}
+		builder.append("Valor total:");
+		builder.append(nf.format(getValorTotal()));
+		return builder.toString();
 	}
 
 }
