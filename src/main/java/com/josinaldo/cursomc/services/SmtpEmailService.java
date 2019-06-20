2e325affdf157df82893568ddfa6aa97f338d560
Josinaldo Gomes <josinaldorobertorocha@gmail.com>
2019-06-19 21:37:58 -0300

    E-mail HTML implementado.

--- src/main/java/com/josinaldo/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/josinaldo/cursomc/services/SmtpEmailService.java
@@ -1,10 +1,13 @@
 package com.josinaldo.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 
@@ -13,11 +16,22 @@ public class SmtpEmailService extends AbstractEmailService {
 	@Autowired
 	private MailSender mailSender;
 	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
-		LOG.info("Envio=ando o e-mail...");
+		LOG.info("Enviando o e-mail...");
 		mailSender.send(msg);
 		LOG.info("E-mail enviado!");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando o e-mail HTML...");
+		javaMailSender.send(msg);
+		LOG.info("E-mail enviado!");
+		
 	}
 
 }
